fa342cc3e0edf792449425394dd5d93008d88c8e
Frederico Rabelo <frederico.rabelo@triasoftware.com>
2023-09-05 18:34:44 -0300

    Iniciando backend

---
+++ src/main/java/br/gov/sp/fatec/recrutatech/RecrutaTechApplication.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.recrutatech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecrutaTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecrutaTechApplication.class, args);
+	}
+
+}

a1a770618af5a910a4b00d1ca529df78db6e9671
Frederico Rabelo <frederico.rabelo@triasoftware.com>
2023-10-04 18:42:22 -0300

    Criando tabelas do CHA

---
+++ src/main/java/br/gov/sp/fatec/recrutatech/entity/Skills.java
@@ -0,0 +1,45 @@
+package br.gov.sp.fatec.recrutatech.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Size;
+
+public class Skills {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Size(max = 64)
+    @Column(unique = true)
+    private String name;
+
+    @Size(max = 255)
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}

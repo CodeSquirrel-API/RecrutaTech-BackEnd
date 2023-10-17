d509f3c754f97962d84d33f9b8dcf1e2a06c026c
LuccaSantiagoDev <luccasantdev@gmail.com>
2023-10-17 20:22:14 -0300

    Login token criando

--- src/main/java/br/gov/sp/fatec/recrutatech/controller/AuthenticationController.java
+++ src/main/java/br/gov/sp/fatec/recrutatech/controller/AuthenticationController.java
@@ -4,16 +4,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.gov.sp.fatec.recrutatech.dto.Login;
 import br.gov.sp.fatec.recrutatech.entity.User;
 import br.gov.sp.fatec.recrutatech.service.authentication.TokenService;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+@CrossOrigin
 @RestController
+@RequestMapping(value = "/login") 
 public class AuthenticationController {
     
     @Autowired
@@ -21,12 +27,18 @@ public class AuthenticationController {
 
     @Autowired
     private TokenService tokenService;
+
+    private static final Logger logger = LoggerFactory.getLogger(AuthenticationController.class);
+
+
     @PostMapping("/login")
     public String login (@RequestBody Login login){
 
+        logger.info("Received Login object: {}-{}", login.getEmail(),login.getPassword());
 
-        
-        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(login.email(),login.password());
+
+
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(login.getEmail(),login.getPassword());
         Authentication authentication =  this.authenticationManager.authenticate(usernamePasswordAuthenticationToken);
 
         var usuario = (User) authentication.getPrincipal();

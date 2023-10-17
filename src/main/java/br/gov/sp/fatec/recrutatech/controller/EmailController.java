b34e9f4f483b57aaf7820da1e4800d2ce05b3930
Frederico Rabelo <frederico.rabelo@triasoftware.com>
2023-10-17 20:11:26 -0300

    Enviando código de autenticação

--- src/main/java/br/gov/sp/fatec/recrutatech/controller/EmailController.java
+++ src/main/java/br/gov/sp/fatec/recrutatech/controller/EmailController.java
@@ -1,9 +1,16 @@
 package br.gov.sp.fatec.recrutatech.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.gov.sp.fatec.recrutatech.dto.EmailDto;
+import br.gov.sp.fatec.recrutatech.service.email.EmailService;
 import io.swagger.annotations.Api;
 
 @RestController
@@ -12,16 +19,18 @@ import io.swagger.annotations.Api;
 @Api(tags = "Email")
 public class EmailController {
 
-    // @Autowired
-    // private EmailService emailService;
+    @Autowired
+    private EmailService emailService;
 
-    // @PostMapping("/validar-code")
-    // public void sendEmailCode(@RequestBody EmailDto email) {
-        // try {
-        //     emailService.sendEmailAuth(email);
-        // } catch (MessagingException e) {
-        //     e.printStackTrace();
-        // }
+    @PostMapping("/send")
+    public ResponseEntity<String> sendEmail(@RequestBody EmailDto email) {
+        try {
+            emailService.sendVerificationCodeEmail(email);
+            return ResponseEntity.ok("Email enviado com sucesso!");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Erro ao enviar o email: " + e.getMessage());
+        }
+    }
 
-    // }
 }
--- src/main/java/br/gov/sp/fatec/recrutatech/service/email/EmailService.java
+++ src/main/java/br/gov/sp/fatec/recrutatech/service/email/EmailService.java
@@ -1,33 +1,60 @@
 package br.gov.sp.fatec.recrutatech.service.email;
 
+import java.util.Arrays;
+import java.util.Random;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
-    
-import br.gov.sp.fatec.recrutatech.dto.emailDto;
-import jakarta.mail.MessagingException;
+
+import br.gov.sp.fatec.recrutatech.config.EmailConfig;
+import br.gov.sp.fatec.recrutatech.dto.EmailDto;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class EmailService {
 
-    private final JavaMailSender sender;
+    // @Value("${spring.mail.username}")
+    // private String supportEmail;
 
-    @Value("${spring.mail.username}")
-    private String supportEmail;
+    // @Value(value = "${spring.mail.host}")
+    // private String smtpHost;
 
-    public void sendEmailAuth(emailDto email) throws MessagingException {
+    // @Value(value = "${spring.mail.port}")
+    // private String smtpPort;
 
-        SimpleMailMessage message = new SimpleMailMessage();
+    public void sendVerificationCodeEmail(EmailDto email) {
+    EmailConfig emailConfig = new EmailConfig();
+    JavaMailSender emailSender = emailConfig.javaMailSender();
+    SimpleMailMessage message = new SimpleMailMessage();
 
-        message.setFrom(supportEmail);
-        message.setSubject(email.getSubject());
-        message.setText(email.getContent());
-        message.setTo(email.getEmail());
+    message.setTo(email.getEmail());
+    message.setSubject("Código de verificação");
 
-        sender.send(message);
+    Integer[] randomNumbers = generateRandomNumbers(6); // Gera 6 números aleatórios
+    String code = Arrays.stream(randomNumbers)
+                        .map(Object::toString)
+                        .collect(Collectors.joining(""));
+
+    message.setText("Olá, seu código é: " + code);
+
+    emailSender.send(message);
+
+    System.out.println("Email sent successfully....");
+}
+
+
+    public static Integer[] generateRandomNumbers(Integer count) {
+        Random random = new Random();
+        Integer[] randomNumbers = new Integer[count];
+
+        for (int i = 0; i < count; i++) {
+            randomNumbers[i] = random.nextInt(10);
+        }
+
+        return randomNumbers;
     }
-
 }

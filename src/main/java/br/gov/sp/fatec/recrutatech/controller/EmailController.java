24d7245a6546c0add727e69e13d33a677981b983
Frederico Rabelo <frederico.rabelo@triasoftware.com>
2023-10-17 20:22:43 -0300

    verificando código do email

--- src/main/java/br/gov/sp/fatec/recrutatech/controller/EmailController.java
+++ src/main/java/br/gov/sp/fatec/recrutatech/controller/EmailController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.gov.sp.fatec.recrutatech.dto.EmailDto;
@@ -19,10 +20,11 @@ import io.swagger.annotations.Api;
 @Api(tags = "Email")
 public class EmailController {
 
+    
     @Autowired
     private EmailService emailService;
 
-    @PostMapping("/send")
+    @PostMapping("/send-code")
     public ResponseEntity<String> sendEmail(@RequestBody EmailDto email) {
         try {
             emailService.sendVerificationCodeEmail(email);
@@ -30,6 +32,18 @@ public class EmailController {
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body("Erro ao enviar o email: " + e.getMessage());
+        }
+    }
+
+    @PostMapping("/check-code")
+    public ResponseEntity<String> verificarCodigo(
+            @RequestParam String email,
+            @RequestParam Integer codigo) {
+
+        if (emailService.checkCode(email, codigo)) {
+            return ResponseEntity.ok("Código correto!");
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Código incorreto!");
         }
     }
 
--- src/main/java/br/gov/sp/fatec/recrutatech/service/email/EmailService.java
+++ src/main/java/br/gov/sp/fatec/recrutatech/service/email/EmailService.java
@@ -1,10 +1,10 @@
 package br.gov.sp.fatec.recrutatech.service.email;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Random;
 import java.util.stream.Collectors;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -17,6 +17,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class EmailService {
 
+    private final HashMap codigos = new HashMap();
+
     // @Value("${spring.mail.username}")
     // private String supportEmail;
 
@@ -27,25 +29,36 @@ public class EmailService {
     // private String smtpPort;
 
     public void sendVerificationCodeEmail(EmailDto email) {
-    EmailConfig emailConfig = new EmailConfig();
-    JavaMailSender emailSender = emailConfig.javaMailSender();
-    SimpleMailMessage message = new SimpleMailMessage();
+        EmailConfig emailConfig = new EmailConfig();
+        JavaMailSender emailSender = emailConfig.javaMailSender();
+        SimpleMailMessage message = new SimpleMailMessage();
 
-    message.setTo(email.getEmail());
-    message.setSubject("Código de verificação");
+        message.setTo(email.getEmail());
+        message.setSubject("Código de verificação");
 
-    Integer[] randomNumbers = generateRandomNumbers(6); // Gera 6 números aleatórios
-    String code = Arrays.stream(randomNumbers)
-                        .map(Object::toString)
-                        .collect(Collectors.joining(""));
+        Integer[] randomNumbers = generateRandomNumbers(6); // Gera 6 números aleatórios
+        String code = Arrays.stream(randomNumbers)
+                .map(Object::toString)
+                .collect(Collectors.joining(""));
 
-    message.setText("Olá, seu código é: " + code);
+        message.setText("Olá, seu código é: " + code);
+        codigos.put(email.getEmail(), Integer.parseInt(code));
 
-    emailSender.send(message);
+        emailSender.send(message);
 
-    System.out.println("Email sent successfully....");
-}
+        System.out.println("Email sent successfully....");
+    }
 
+    public boolean checkCode(String email, Integer codigo) {
+        Object codigoArmazenado = codigos.get(email);
+
+        if (codigoArmazenado != null) {
+            Integer codigoArmazenadoInt = (Integer) codigoArmazenado;
+            return codigo.equals(codigoArmazenadoInt);
+        }
+
+        return false;
+    }
 
     public static Integer[] generateRandomNumbers(Integer count) {
         Random random = new Random();

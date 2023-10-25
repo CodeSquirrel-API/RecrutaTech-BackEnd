8532720d9d2ffdc6a3edc748d4844df1741f9422
LuccaSantiagoDev <luccasantdev@gmail.com>
2023-10-25 18:02:50 -0300

    CrossOrigin bug

--- src/main/java/br/gov/sp/fatec/recrutatech/controller/CandidateController.java
+++ src/main/java/br/gov/sp/fatec/recrutatech/controller/CandidateController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +26,7 @@ import br.gov.sp.fatec.recrutatech.service.knowledge.KnowledgeService;
 import br.gov.sp.fatec.recrutatech.service.skill.SkillService;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/candidates")
 public class CandidateController {
 
